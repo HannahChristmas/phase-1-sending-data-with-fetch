dd92c2604fc5cecebea0ac99af09485e2d113804
Hannah Christmann <hc109909@gmail.com>
2022-08-11 11:35:01 -0400

    commit tree bc3c22c27d21a331eae57279e2c8eb8888ed0632

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Add your code here
+// const submitData = {
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//         name: '',
+//         email: '' 
+//     }),
+//   };
+  
+  
+//   fetch("http://localhost:3000/users", submitData)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     })
+    // .catch(function (error) {
+    //   alert("Bad things! Ragnarők!");
+    //   console.log(error.message);
+    // });
+
+
+    function submitData(userName, userEmail){
+        return fetch("http://localhost:3000/users",{
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body:JSON.stringify({
+                name: userName,
+                email: userEmail
+            })
+        
+        })
+        //getting the response from GOT
+        .then(function (response) {
+            //translating the response for GOT 
+            return response.json();
+          })
+          //reading the translated response
+        .then(function (data){
+            //
+            const body = document.querySelector('body');
+            document.body.append(data.id);
+        })
+        .catch(function (error) {
+            document.body.append(error.message);
+          })
+    }
+
+
+    // How can we set the text of the body to be the response.id so it shows up in the DOM
